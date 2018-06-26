04a83df0b9c343f9dad3de211b2f115cfe445988
kimtis <kimtis@naver.com>
2018-06-26 23:56:41 +0900

    [KTIS-000] Initial commit

---
+++ src/main/java/com/kimtis/shorturl/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.kimtis.shorturl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("now", new Date());
+        return "index";
+    }
+}
