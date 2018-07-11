6b87f62b262fd32a7036f4863f93a42ea60fe2df
kimtis <kimtis@naver.com>
2018-07-11 23:10:56 +0900

    [KTIS-006] remove lamda expression

--- src/main/java/com/kimtis/shorturl/service/ShortUrlJdbcRepository.java
+++ src/main/java/com/kimtis/shorturl/service/ShortUrlJdbcRepository.java
@@ -5,11 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Slf4j
@@ -24,13 +29,16 @@ public class ShortUrlJdbcRepository {
     public ShortUrl findById(long id) {
         return singleResultOrNull(
             jdbcTemplate.query(QUERY_READ, new Object[]{id},
-                (rs, rowNum) -> (
-                    ShortUrl.builder()
-                        .id(rs.getLong("ID"))
-                        .link(rs.getString("LINK"))
-                        .status(rs.getInt("STATUS"))
-                        .build()
-                )
+                new RowMapper<ShortUrl>() {
+                    @Override
+                    public ShortUrl mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return ShortUrl.builder()
+                                .id(rs.getLong("ID"))
+                                .link(rs.getString("LINK"))
+                                .status(rs.getInt("STATUS"))
+                                .build();
+                    }
+                }
             )
         );
     }
@@ -38,12 +46,15 @@ public class ShortUrlJdbcRepository {
     public ShortUrl save(ShortUrl shortUrl) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         int resultCode = jdbcTemplate.update(
-            conn -> {
-                PreparedStatement ps = conn.prepareStatement(QUERY_CREATE, new String[]{"ID"});
-                ps.setString(1, shortUrl.getLink());
-                ps.setInt(2, shortUrl.getStatus());
-                return ps;
-            },
+            new PreparedStatementCreator() {
+                 @Override
+                 public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+                     PreparedStatement ps = conn.prepareStatement(QUERY_CREATE, new String[]{"ID"});
+                     ps.setString(1, shortUrl.getLink());
+                     ps.setInt(2, shortUrl.getStatus());
+                     return ps;
+                 }
+             },
             keyHolder
         );
 
